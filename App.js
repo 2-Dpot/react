678b261e8efa6a5c24fb1fae4b98be7dc4fa33d6
unknown <sarit.s.nayak@gmail.com>
2023-07-19 01:44:34 +0530

    dist folder add by build

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 /*jshint esversion: 11 */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement('div', {data:222}, [React.createElement('h1', {data:111}, 'Hello World React'),
+const heading = React.createElement('div', {data:222}, [React.createElement('h1', {data:111}, 'Hello World React 🚀'),
 React.createElement('h1', {data:111}, 'Hello World React1')]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
